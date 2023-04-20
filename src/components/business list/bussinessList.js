60fcece411cedc422135e9405a055daeb92ddb73
namyrrepa <perryman.austinm@gmail.com>
2023-04-20 13:57:04 -0400

    business list component

---
+++ src/components/business list/bussinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Business from '../business/business.js';
+
+const BusinessList = () => {
+    return (
+        <div>
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    );
+};
+
+export default BusinessList;
